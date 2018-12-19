ccf17ada1c109d946fac0102a8e8d831746819d2
Devollin <34558845+Devollin@users.noreply.github.com>
2018-12-19 12:44:08 -0600

    Update DAM.lua

--- DAM.lua
+++ DAM.lua
@@ -1,1 +1,18 @@
-local function Synthesizer
+-- Development Assistance Module 0.1.0 / Coded by Devollin / Started 12.19.18
+
+-- Synthesizer 1.0.0 / Edited 12.19.18
+local function Synthesizer(Obj, Data)
+  assert(Obj and Data, 'No parameters were filled!')
+  Obj = (typeof(Obj) == 'Instance' and Obj) or Instance.new(Obj)
+  for Index, Prop in pairs(Data) do
+    if Index ~= 'Parent' then
+      Obj[Index] = Prop
+    end
+  end
+  if Data.Parent then
+    Obj.Parent = Data.Parent
+  end
+  return Obj
+end
+
+return Synthesizer
