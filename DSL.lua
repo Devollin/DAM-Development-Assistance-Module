fe4823b1cab16be3d7fe6580447b91859acdd436
Devollin <34558845+Devollin@users.noreply.github.com>
2018-12-19 22:12:15 -0600

    Update DSL.lua

--- DSL.lua
+++ DSL.lua
@@ -1,17 +1,15 @@
 -- Development Support Library 0.1.0 / Coded by Devollin / Started 12.19.18
 
 -- Synthesize 1.0.0 / Edited 12.19.18
-local function Synthesize(Obj, Data)
-  assert(Obj and Data, 'No parameters were filled!')
+local function Synthesize(Obj, Properties)
+  assert(Obj and Properties, 'No parameters were filled!')
+  local Parent = Properties.Parent or nil
+  Properties.Parent = nil
   Obj = (typeof(Obj) == 'Instance' and Obj) or Instance.new(Obj)
-  for Index, Prop in pairs(Data) do
-    if Index ~= 'Parent' then
-      Obj[Index] = Prop
-    end
+  for Index, Prop in pairs(Properties) do
+    Obj[Index] = Prop
   end
-  if Data.Parent then
-    Obj.Parent = Data.Parent
-  end
+  Obj.Parent = Parent or Obj.Parent
   return Obj
 end
 
