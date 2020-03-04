0cd352fa15ae7abb8fd396bfb47621afc5ad7801
Devollin <34558845+Devollin@users.noreply.github.com>
2020-03-04 13:07:40 -0600

    Update DSL.lua

--- DSL.lua
+++ DSL.lua
@@ -46,13 +46,11 @@ end
 --// Weld 1.0.1 / Edited 2.1.20
 function DSL.Weld(Model, CorePart, Ignore)
 	function Recursive(Collection)
-		for Ind, Child in pairs(Collection:GetChildren()) do
+		for Ind, Child in pairs(Collection:GetDescendants()) do
 			if Child ~= Ignore and Child ~= CorePart then
 				if Child:IsA("BasePart") then
 					DSL.Synth({Part0 = CorePart, Part1 = Child, Name = Child.Name, Parent = CorePart})
 					Child.Anchored = false
-				elseif (Child:IsA("Folder") or Child:IsA("Model")) then
-					Recursive(Child)
 				end
 			end
 		end
