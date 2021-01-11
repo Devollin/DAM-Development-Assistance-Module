343722ea1a8ff8c207cf15c48d2cf2949ca2c09e
Devollin <34558845+Devollin@users.noreply.github.com>
2021-01-11 11:48:39 -0600

    V0.6.0
    -Formatting overhaul
    -Added DSL.TableMerge(a: table, b: table)
    -Removed DSL.Tween() [spun off into another module, Omnitween: https://github.com/Devollin/Omnitween]
    -Updated parameters for DSL.Synth()
       Previously: DSL.Synth(tree: table {className: string, properties: table {children: table})
       Now: DSL.Synth(object: string | instance | table, properties: table, modifiers: table {callbacks: table {[eventName: string] = callback: function}, children: table {[className: string] = value: any}})
    -Updated parameters for DSL.Center()
       Previously: DSL.Center(gui: instance, mouse: Mouse)
       Now: DSL.Center(absoluteSize: Vector2)
    -Updated parameters for DSL.Weld()
       Previously: DSL.Weld(model: Model, corePart: BasePart, ignore: BasePart)
       Now: DSL.Weld(model: Model, corePart: BasePart, ignore: Model | BasePart | table {value: Model | BasePart})
    -Renamed DSL.GetLengthOfDictionary() to DSL.GetDictionaryLength()
    -Submodules are no longer iterated through if they returned a table, instead, the entire table is indexed by the name of the submodule

--- DSL.lua
+++ DSL.lua
@@ -1,165 +1,147 @@
---// Development Support Library 0.5.1 / Coded by Devollin / Started 12.19.18
-
+--!strict
+--// Development Support Library 0.6.0 / Coded by Devollin / Started 12.19.18
 local DSL = {}
-local TS = game:GetService('TweenService')
 
---// Synth 1.2.0 / Edited 2.10.20
-function Recursive(Obj, Properties)
-	local Parent, Children = Properties.Parent or nil, Properties.Children or {}
-	Properties.Parent, Properties.Children = nil, nil
-	Obj = (typeof(Obj) == 'Instance' and Obj) or Instance.new(Obj)
-	for Index, Prop in pairs(Properties) do
-		Obj[Index] = Prop
-	end
-	for Index, Child in pairs(Children) do
-		Child[2].Name = Child[2].Name or Index
-		Child[2].Parent = Obj
-		DSL.Synth(Child[1], Child[2])
-	end
-	if Obj.Parent ~= game then
-		Obj.Parent = Parent or Obj.Parent
-	end
-	return Obj
-end
-
-function DSL.Synth(Obj, Properties)
-	assert(Obj and Properties, 'No parameters were filled!')
-	if typeof(Obj) == 'table' then
-		local Objects = {}
-		for Index, Ob in pairs(Obj) do
-			Recursive(Ob, Properties)
+--// Synth 1.4.0 / Edited 1.11.21
+function recursive(object: any, properties: any, modifiers: any)
+	local final = (typeof(object) == "Instance" and object) or Instance.new(object)
+	for name, property in pairs(properties) do
+		if final[name] ~= nil and name ~= "Parent" then
+			final[name] = property
 		end
-		return Objects
+	end
+	for class, data in pairs(modifiers.children) do
+		if not final:FindFirstChild(class) then
+			data.properties.Name = class
+		end
+		data.properties.Parent = final
+		DSL.Synth(class, data.properties, data.modifiers)
+	end
+	for name, event in pairs(modifiers.callbacks) do
+		final[name]:Connect(function(...)
+			event(final, ...)
+		end)
+	end
+	if final.Parent and final.Parent ~= game then
+		final.Parent = properties.Parent or final.Parent
+	end
+	return final
+end
+
+function DSL.Synth(class: string, properties: any, modifiers: any)
+	assert(class, "No parameters were filled!", debug.traceback())
+	assert(properties, "No properties were provided!", debug.traceback())
+	if typeof(class) == "string" and typeof(properties) == "table" then
+		modifiers = modifiers or {callbacks = {}, children = {}}
+		return recursive(class, properties, modifiers)
 	else
-		return Recursive(Obj, Properties)
+		error("Invalid setup!")
 	end
 end
 
---// Weld 1.0.3 / Edited 3.24.20
-function DSL.Weld(Model, CorePart, Ignore)
-	for Ind, Child in pairs(Model:GetDescendants()) do
-		if Child ~= Ignore and Child ~= CorePart then
-			if Child:IsA("BasePart") then
-				DSL.Synth('WeldConstraint', {Part0 = CorePart, Part1 = Child, Name = Child.Name, Parent = CorePart})
-				Child.Anchored = false
+--// Weld 1.1.0 / Edited 1.11.21
+function DSL.Weld(model: Model, corePart: BasePart, ignore: any)
+	local ignoreList = {}
+	if ignore and typeof(ignore) == "Instance" then
+		if ignore:IsA("Model") then
+			for _, ignored in pairs(ignore:GetDescendants()) do
+				if ignored:IsA("BasePart") then
+					table.insert(ignoreList, ignored)
+				end
+			end
+		elseif ignore:IsA("BasePart") then
+			table.insert(ignoreList, ignore)
+		end
+	elseif ignore and typeof(ignore) == "table" then
+		for _, ignored in pairs(ignore) do
+			if ignored:IsA("Model") then
+				for _, ignoring in pairs(ignore:GetDescendants()) do
+					if ignoring:IsA("BasePart") then
+						table.insert(ignoreList, ignoring)
+					end
+				end
+			elseif ignored:IsA("BasePart") then
+				table.insert(ignoreList, ignored)
 			end
 		end
 	end
-end
-
---// Tween 1.1.2 / Edited 2.13.20
-local T = {}
-T.__index = T
-
-local function Pure(Obj, Prop, Dat)
-	return TS:Create(Obj, TweenInfo.new(Dat.T, Dat.ES, Dat.ED, Dat.RC, Dat.R, Dat.DT), Prop)
-end
-
-function T.new(Data)
-	return setmetatable({Data = Data, Completed = Data[1].Completed}, T)
-end
-function T:Cancel()
-	for Index, TN in pairs(self.Data) do TN:Cancel() end
-end
-function T:Pause() 
-	for Index, TN in pairs(self.Data) do TN:Pause() end
-end
-function T:Play()
-	for Index, TN in pairs(self.Data) do TN:Play() end
-end
-
-local Info = {
-	['Instance'] = function(Object, Properties, Data)
-		return Pure(Object, Properties, Data)
-	end,
-	['table'] = function(Objects, Properties, Data)
-		local Tweens = {}
-		for Index, Object in pairs(Objects) do
-			table.insert(Tweens, 1, Pure(Object, Properties, Data))
+	for _, child in pairs(model:GetDescendants()) do
+		if child:IsA("BasePart") and (not table.find(ignore or {}, child)) and child ~= corePart then
+			DSL.Synth("WeldConstraint", {Part0 = corePart, Part1 = child, Name = child.Name, Parent = corePart}, nil)
+			child.Anchored = false
 		end
-		return T.new(Tweens)
-	end,
-	[true] = function(Tween) Tween:Play() end,
-	[false] = function(Tween) Tween:Cancel() end
-}
-
-function DSL.Tween(Object, Properties, Data)
-	 local Success, Ret = pcall(function()
-		assert(Object and Properties and typeof(Properties) == "table", "Insufficient values, cancelling Tween!")
-		local Data, Tween = Data or {}, nil
-		Data.T = Data.T or .25 -- Time
-		Data.ES = Data.ES and Enum.EasingStyle[Data.ES] or Enum.EasingStyle.Quart -- EasingStyle
-		Data.ED = Data.ED and Enum.EasingDirection[Data.ED] or Enum.EasingDirection.Out -- EasingDirection
-		Data.RC = Data.RC or 0 -- RepeatCount
-		Data.R = Data.R or false -- Repeat
-		Data.DT = Data.DT or 0 -- DelayTime
-		Data.AP = Data.AP or true -- AutoPlay
-		Data.CB = Data.CB or false -- Callback
-		local Set = Info[typeof(Object)](Object, Properties, Data)
-		Info[Data.AP](Set)
-		return Set
-	end)
-	if Success then
-		return Ret
 	end
-	warn('Tween failed:'..Ret)
 end
 
---// Magnitude 1.0.0 / Edited 8.31.19
-function DSL.Magnitude(Start, End)
-	return math.sqrt((End.X - Start.X) ^ 2 + (End.Y - Start.Y) ^ 2 + (End.Z - Start.Z) ^ 2)
+--// Magnitude 1.0.1 / Edited 1.11.21
+function DSL.Magnitude(a: Vector3, b: Vector3)
+	return math.sqrt((b.X - a.X) ^ 2 + (b.Y - a.Y) ^ 2 + (b.Z - a.Z) ^ 2)
 end
 
---// Round 1.0.2 / Edited 6.3.20
-function DSL.Round(Number, Place)
-	local Adjust = 10 ^ (Place or 0)
-	return math.ceil((Number * Adjust) - 0.5) / Adjust
+--// Round 1.0.3 / Edited 1.11.21
+function DSL.Round(number: number, place: number)
+	local adjust = 10 ^ (place or 0)
+	return math.ceil((number * adjust) - 0.5) / adjust
 end
 
---// Center 1.0.0 / Edited 2.10.20
-function DSL.Center(Gui, Mouse)
-	return Vector2.new(Mouse.X - (Gui.AbsoluteSize.X / 2), Mouse.Y - (Gui.AbsoluteSize.Y / 2))
+--// Center 1.0.1 / Edited 1.11.21
+function DSL.Center(absoluteSize: Vector2)
+	local mouse = game:GetService("Players").LocalPlayer:GetMouse()
+	return Vector2.new(mouse.X - (absoluteSize.X / 2), mouse.Y - (absoluteSize.Y / 2))
 end
 
---// Pythagorean 1.0.1 / Edited 5.29.20
-function DSL.Pythagorean(A, B)
-	return math.sqrt((A ^ 2) + (B ^ 2))
+--// Pythagorean 1.0.2 / Edited 1.11.21
+function DSL.Pythagorean(a: number, b: number)
+	return math.sqrt((a ^ 2) + (b ^ 2))
 end
 
---// Shorthand 1.0.3 / Edited 6.3.20 / By Algoritimi
-function DSL.Shorthand(Number)
-	local Index = math.clamp(math.floor(math.log10(Number) / 3), 0, 4)
-	local Shorthand = {
-		[0] = '',
-		[1] = "k",
-		[2] = "m",
-		[3] = "b",
-		[4] = 't'
-	}
-	local Place = Index == 0 and 0 or 1
-	return Shorthand[Index] and tostring(DSL.Round(Number / (1000 ^ Index), Place)) .. Shorthand[Index] or Number
+--// Shorthand 1.0.4 / Edited 1.11.21 / By Algoritimi
+function DSL.Shorthand(number: number)
+	local index, shorthand = math.clamp(math.floor(math.log10(number) / 3), 0, 4), {"", "k", "m", "b", "t"}
+	return shorthand[index + 1] and tostring(DSL.Round(number / (1000 ^ index), (index == 0 and 0 or 1))) .. shorthand[index + 1] or number
 end
 
---// GetLengthOfDictionary 1.0.0 / Edited 5.29.20
-function DSL.GetLengthOfDictionary(Dictionary)
-	local Length = 0
-	for Index, Item in pairs(Dictionary) do
-		Length = Length + 1
+--// GetDictionaryLength 1.0.1 / Edited 1.11.21
+function DSL.GetDictionaryLength(dictionary: any)
+	local length = 0
+	for _, _ in pairs(dictionary) do
+		length += 1
 	end
-	return Length
+	return length
+end
+
+--// TableMerge 1.0.0 / Edited 1.11.21
+function DSL.TableMerge(a: any, b: any)
+	local c = {}
+	for index, value in pairs(a) do
+		if type(value) == "table" then
+			if c[index] == nil then
+				c[index] = DSL.TableMerge({}, value)
+			else
+				c[index] = DSL.TableMerge(c[index], value)
+			end
+		else
+			c[index] = value
+		end
+	end
+	for index, value in pairs(b) do
+		if type(value) == "table" then
+			if c[index] == nil then
+				c[index] = DSL.TableMerge({}, value)
+			else
+				c[index] = DSL.TableMerge(c[index], value)
+			end
+		else
+			c[index] = value
+		end
+	end
+	return c
 end
 
 
 --// Setup
-for Index, Child in pairs(script:GetChildren()) do
-	local Required = require(Child)
-	if typeof(Required) == 'table' then
-		for FuncName, Func in pairs(Required) do
-			DSL[FuncName] = Func
-		end
-	elseif typeof(Required) == 'function' then
-		DSL[Child.Name] = Required
-	end
+for _, child in pairs(script:GetChildren()) do
+	DSL[child.Name] = require(child)
 end
 
 return DSL
